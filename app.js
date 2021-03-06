b1095f2bbe9d8c9b83ecd968918a155bd27ebcc7
ElliotCase <40835798+ElliotCase@users.noreply.github.com>
2021-03-06 21:40:05 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('shown')
     menu.classList.toggle('show');
 })
+
+var counter = 1;
+setInterval(function () {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 3000);
